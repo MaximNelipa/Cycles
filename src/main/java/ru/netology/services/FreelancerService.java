aeb57060a0ff3f644d792c4f8999a372ffde7f18
MAX <max_nelipa@mail.ru>
2023-04-05 00:23:33 +0300

    Gitignore added

---
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -0,0 +1,25 @@
+package ru.netology.services;
+
+public class FreelancerService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int counter = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                //             money = money - expenses;
+                money -= expenses;
+                //               money = money/3;
+                money /= 3;
+                counter++;
+            } else {
+                money += income;
+                money -= expenses;
+            }
+
+
+        }
+        return counter;
+
+    }
+}
